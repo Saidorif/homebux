6485513e1f495360145075477781551278f2952f
Saidorif <sayyid2112@gmail.com>
2018-11-15 18:12:44 +0500

    added some vendors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,12 +85,12 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/about">About us</a>
+                    <a href="/about">Laracasts</a>
+                    <a href="/about">News</a>
+                    <a href="/about">Nova</a>
+                    <a href="/about">Forge</a>
+                    <a href="/about">GitHub</a>
                 </div>
             </div>
         </div>
